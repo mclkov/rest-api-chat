b2d968e0dd63e70490d60d3c34d34e5271ba4c95
McL <mcl@brm>
2018-08-19 16:13:57 +0300

    end of lesson 2

---
+++ smack/Controller/ChatVC.swift
@@ -0,0 +1,43 @@
+//
+//  ChatVC.swift
+//  smack
+//
+//  Created by McL on 8/19/18.
+//  Copyright © 2018 McL. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
