cb71641d94e371ba8d3da291ffd01bc1b9e4ab45
McL <mcl@brm>
2018-08-23 10:07:41 +0300

    AuthService, UserDefaults, Completion Handler, Closure, first POST request through Alamofire

--- smack/Utilities/Constants.swift
+++ smack/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://chatappo.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 let LOGIN_VC = "login_vc"
 let REGISTRATION_VC = "registration_vc"
 let UNWIND = "unwind_to_channel_vc"
+
+// User defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
