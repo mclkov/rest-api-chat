6c2de425b6dd6a2ad2abdf19eda64286a89383f5
McL <mcl@brm>
2018-09-10 18:32:24 +0300

    MessageService

---
+++ smack/Services/MessageService.swift
@@ -0,0 +1,15 @@
+//
+//  MessageService.swift
+//  smack
+//
+//  Created by McL on 9/10/18.
+//  Copyright © 2018 McL. All rights reserved.
+//
+
+import Foundation
+
+class MessageService
+{
+    static let instance = MessageService()
+    
+}
